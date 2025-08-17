512f0ebf82d6bda44b3321cce3bcebe2798c5bb9
Raihan Muhammad Riswandi <165734611+Raihan220106@users.noreply.github.com>
2025-08-17 19:28:13 +0700

    Update web.php
    Signed-off-by: Raihan Muhammad Riswandi  <165734611+Raihan220106@users.noreply.github.com>

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,16 @@ Route::get('/', function () {
     return redirect()->route('distributions.index');
 });
 
-Route::resource('distributions', DistributionController::class);
-
-Route::get('/distribution-products', [DistributionProductController::class, 'index']);
-Route::post('/distribution-products', [DistributionProductController::class, 'store']);
-Route::delete('/distribution-products/{id}', [DistributionProductController::class, 'destroy']);
-
-// login & logout
+// Routes login (public)
 Route::get('/login', [AuthManualController::class,'index'])->name('login');
 Route::post('/login', [AuthManualController::class,'loginProses'])->name('loginProses');
 Route::post('/logout', [AuthManualController::class,'logout'])->name('logout');
+
+// Routes yang hanya bisa diakses setelah login
+Route::middleware('auth')->group(function () {
+    Route::resource('distributions', DistributionController::class);
+
+    Route::get('/distribution-products', [DistributionProductController::class, 'index']);
+    Route::post('/distribution-products', [DistributionProductController::class, 'store']);
+    Route::delete('/distribution-products/{id}', [DistributionProductController::class, 'destroy']);
+});
